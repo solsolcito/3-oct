8f64fd7bd92413ad359e75b43f2f0679846f16f9
solange alarcon <solealarconmelinao@gmail.com>
2022-10-03 11:00:15 -0300

    clase del 3 oct funciones

---
+++ main.js
@@ -0,0 +1,32 @@
+const saludar =() =>{
+console.log("hola a todos")
+
+}
+
+const getsalute = (name)=>{
+return "hola" + name
+}
+
+//saludar()
+//console.log(getsalute("solange"))
+//console.log(getsalute("babby"))
+//console.log("hola")
+
+const getsalute2 = (name,name2,lastname,lastname2)=>
+{
+    return name+ " " + name2+ " " + lastname+ " " + lastname2
+}
+
+console.log(getsalute2("solange","andrea","alarcon","melinao"))
+
+
+const espar = (number) => { 
+    if ( number % 2 == 0){
+        return true
+
+    } else {
+        return false
+    }
+
+}
+console.log(espar(4))
